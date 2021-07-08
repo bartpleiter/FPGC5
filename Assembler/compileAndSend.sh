fd8651501af5fd60af4282c29f3b765dc9f0c8d7
Bart <bart@b4rt.nl>
2021-07-08 11:38:37 +0200

    Added compileAndSend script in Assembler. Renamed the simTest file

---
+++ Assembler/compileAndSend.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Assembling B332 ASM code"
+if (python3 Assembler.py > ../Programmer/code.list) # compile and write to code.list in Programmer folder
+then
+        echo "B332 ASM code successfully assembled"
+        # convert list to binary files and send to FPGC
+
+        # WSL1/Linux version
+        #if [[ $1 == "flash" ||  $1 == "write" ]]
+        #then
+        #    (cd ../Programmer && bash compileROM.sh && echo "Flashing binary to FPGC flash" && python3 flash.py write)
+        #else
+        #    (cd ../Programmer && bash compileROM.sh noPadding && echo "Sending binary to FPGC" && python3 uartFlasher.py)
+        #fi
+
+        # WSL2/Windows version
+        if [[ $1 == "flash" ||  $1 == "write" ]]
+        then
+            (cd ../Programmer && bash compileROM.sh && echo "Flashing binary to FPGC flash" && python.exe flash_win.py write)
+        else
+            (cd ../Programmer && bash compileROM.sh noPadding && echo "Sending binary to FPGC" && python.exe uartFlasher_win.py)
+        fi
+
+else # assemble failed, run again to show error
+    echo "Failed to assemble B332 ASM code"
+    python3 Assembler.py
+fi
