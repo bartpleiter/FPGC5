178a12796a08f1890679e8d4d256a82e088cf979
Bart <bart@b4rt.nl>
2021-12-05 17:47:32 +0100

    Ported wget over to BCC

--- BCC/userBDOS/wget.c
+++ BCC/userBDOS/wget.c
@@ -1,7 +1,13 @@
-#include "lib/stdlib.h"
-#include "lib/sys.h"
-#include "lib/fs.h"
-#include "lib/wiz5500.h"
+// Wget
+// Tool to do a HTTP GET request
+
+#define word char
+
+#include "lib/math.c"
+#include "lib/stdlib.c"
+#include "lib/sys.c"
+#include "lib/fs.c"
+#include "lib/wiz5500.c"
 
 #define HEAP_LOCATION 0x500000
 
@@ -13,35 +19,17 @@
 
 // Initializes w5500 in client TCP mode, with a static ip (last digit as arg)
 // Port is also given as arg
-void initClient(int ip, int port)
+void initClient(word ip, word port)
 {
-  char ip_addr[4];
-  ip_addr[0] = 192;
-  ip_addr[1] = 168;
-  ip_addr[2] = 0;
-  ip_addr[3] = ip;
+  word ip_addr[4] = {192, 168, 0, 213};
 
-  char gateway_addr[4];
-  gateway_addr[0] = 192;
-  gateway_addr[1] = 168;
-  gateway_addr[2] = 0;
-  gateway_addr[3] = 1;
+  word gateway_addr[4] = {192, 168, 0, 1};
 
-  char mac_addr[6];
-  mac_addr[0] = 0xDE;
-  mac_addr[1] = 0xAD;
-  mac_addr[2] = 0xBE;
-  mac_addr[3] = 0xEF;
-  mac_addr[4] = 0x24;
-  mac_addr[5] = 0x64;
+  word mac_addr[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x24, 0x64};
 
-  char sub_mask[4];
-  sub_mask[0] = 255;
-  sub_mask[1] = 255;
-  sub_mask[2] = 255;
-  sub_mask[3] = 0;
+  word sub_mask[4] = {255, 255, 255, 0};
 
-  wiz_Init(&ip_addr[0], &gateway_addr[0], &mac_addr[0], &sub_mask[0]);
+  wiz_Init(ip_addr, gateway_addr, mac_addr, sub_mask);
 
   // Open socket in TCP Client mode at port
   wizInitSocketTCPClient(SOCKET_CLIENT, port);
@@ -50,27 +38,27 @@ void initClient(int ip, int port)
 
 // Connect to target at given ip and port
 // Return 1 on success, 0 on failure
-int connectToTarget(char* ipTarget, int portTarget)
+word connectToTarget(char* ipTarget, word portTarget)
 {
-  wizWrite(SnDIPR,  WIZNET_WRITE_SnR + (SOCKET_CLIENT << 5), &ipTarget[0], 4);
-  wizSetSockReg16(SOCKET_CLIENT, SnDPORT, portTarget);
-  wizCmd(SOCKET_CLIENT, CR_CONNECT);
+  wizWrite(WIZNET_SnDIPR,  WIZNET_WRITE_SnR + (SOCKET_CLIENT << 5), ipTarget, 4);
+  wizSetSockReg16(SOCKET_CLIENT, WIZNET_SnDPORT, portTarget);
+  wizCmd(SOCKET_CLIENT, WIZNET_CR_CONNECT);
 
-  int sxStatus = wizGetSockReg8(SOCKET_CLIENT, SnSR);
+  word sxStatus = wizGetSockReg8(SOCKET_CLIENT, WIZNET_SnSR);
 
-  int retries = 0;
+  word retries = 0;
 
-  while (sxStatus != SOCK_ESTABLISHED && retries < CONNECT_MAX_RETRIES)
+  while (sxStatus != WIZNET_SOCK_ESTABLISHED && retries < CONNECT_MAX_RETRIES)
   {
     //BDOS_PrintHexConsole(sxStatus);
-    if (sxStatus == SOCK_CLOSED)
+    if (sxStatus == WIZNET_SOCK_CLOSED)
     {
       return 0;
     }
 
-    sxStatus = wizGetSockReg8(SOCKET_CLIENT, SnSR);
+    sxStatus = wizGetSockReg8(SOCKET_CLIENT, WIZNET_SnSR);
 
-    if (sxStatus != SOCK_ESTABLISHED)
+    if (sxStatus != WIZNET_SOCK_ESTABLISHED)
     {
       retries++;
       delay(1);
@@ -86,23 +74,23 @@ int connectToTarget(char* ipTarget, int portTarget)
 void sendRequest()
 {
   char* rTxt = (char*) HEAP_LOCATION; // we use heap for the request text
-  int rLen = 0;
-  int catLen = 0;
+  word rLen = 0;
+  word catLen = 0;
 
   catLen = strcpy(rTxt + rLen, "GET /");
   rLen += catLen;
 
   char targetURL[64];
-  BDOS_GetArgN(2, &targetURL[0]);
-  catLen = strcpy(rTxt + rLen, &targetURL[0]);
+  BDOS_GetArgN(2, targetURL);
+  catLen = strcpy(rTxt + rLen, targetURL);
   rLen += catLen;
 
   catLen = strcpy(rTxt + rLen, " HTTP/1.1\r\nUser-Agent: WGET(FPGC)\r\nAccept: */*\r\nHost: ");
   rLen += catLen;
 
   char ipstr[24];
-  BDOS_GetArgN(1, &ipstr[0]);
-  catLen = strcpy(rTxt + rLen, &ipstr[0]);
+  BDOS_GetArgN(1, ipstr);
+  catLen = strcpy(rTxt + rLen, ipstr);
   rLen += catLen;
 
   catLen = strcpy(rTxt + rLen, "\r\n\r\n");
@@ -115,39 +103,39 @@ void sendRequest()
 
 // Init output file for download
 // Returns 1 on success, else 0
-int initOutputFile(char* path, char* fname, char* rbuf, int rsize)
+word initOutputFile(char* path, char* fname, char* rbuf, word rsize)
 {
   // sanity check current path
-  if (FS_sendFullPath(&path[0]) == ANSW_USB_INT_SUCCESS)
+  if (FS_sendFullPath(path) == FS_ANSW_USB_INT_SUCCESS)
   {
-    int retval = FS_open();
+    word retval = FS_open();
     // check that we can open the path
-    if (retval == ANSW_USB_INT_SUCCESS || retval == ANSW_ERR_OPEN_DIR)
+    if (retval == FS_ANSW_USB_INT_SUCCESS || retval == FS_ANSW_ERR_OPEN_DIR)
     {
       // check length of filename
-      if (strlen(&fname[0]) <= 12)
+      if (strlen(fname) <= 12)
       {
         // uppercase filename
-        strToUpper(&fname[0]);
+        strToUpper(fname);
         // send filename
-        FS_sendSinglePath(&fname[0]);
+        FS_sendSinglePath(fname);
 
         // create the file
-        if (FS_createFile() == ANSW_USB_INT_SUCCESS)
+        if (FS_createFile() == FS_ANSW_USB_INT_SUCCESS)
         {
           // open the path again
-          FS_sendFullPath(&path[0]);
+          FS_sendFullPath(path);
           FS_open();
 
           // send filename again
-          FS_sendSinglePath(&fname[0]);
+          FS_sendSinglePath(fname);
           // open the newly created file
-          if (FS_open() == ANSW_USB_INT_SUCCESS)
+          if (FS_open() == FS_ANSW_USB_INT_SUCCESS)
           {
             // set cursor to start
-            if (FS_setCursor(0) == ANSW_USB_INT_SUCCESS)
+            if (FS_setCursor(0) == FS_ANSW_USB_INT_SUCCESS)
             {
-              if (FS_writeFile(&rbuf[0], rsize) != ANSW_USB_INT_SUCCESS)
+              if (FS_writeFile(rbuf, rsize) != FS_ANSW_USB_INT_SUCCESS)
               {
                 FS_close();
                 BDOS_PrintConsole("Error while writing to file\n");
@@ -190,11 +178,11 @@ int initOutputFile(char* path, char* fname, char* rbuf, int rsize)
 
 
 // Parses HTTP header, returns location of the first data byte
-int getDataStart(char* rbuf, int rsize)
+word getDataStart(char* rbuf, word rsize)
 {
-  int dataStart = 0;
+  word dataStart = 0;
 
-  int i = 0;
+  word i = 0;
   while (i < rsize - 4 && dataStart == 0)
   {
     //BDOS_PrintcConsole(rbuf[i]);
@@ -215,9 +203,9 @@ int getDataStart(char* rbuf, int rsize)
 
 
 // Parses HTTP header, returns status code
-int getStatusCode(char* rbuf, int rsize)
+word getStatusCode(char* rbuf, word rsize)
 {
-  int i = 0;
+  word i = 0;
   // look for first space, then 3 digits
   while (i < rsize - 4)
   {
@@ -228,7 +216,7 @@ int getStatusCode(char* rbuf, int rsize)
       statusStr[1] = rbuf[i+2];
       statusStr[2] = rbuf[i+3];
       statusStr[3] = 0;
-      return strToInt(&statusStr[0]);
+      return strToInt(statusStr);
     }
 
     i++;
@@ -239,10 +227,10 @@ int getStatusCode(char* rbuf, int rsize)
 
 
 // Parses HTTP header, returns content length header field
-int getContentLength(char* rbuf, int rsize)
+word getContentLength(char* rbuf, word rsize)
 {
-  int i = 0;
-  int strBufIndex = 0;
+  word i = 0;
+  word strBufIndex = 0;
   // look for Content-Length field
   while (i < rsize - 4)
   {
@@ -262,7 +250,7 @@ int getContentLength(char* rbuf, int rsize)
 
       contentLengthStrBuf[strBufIndex] = 0; // terminate buffer
       // finally, convert to int and return
-      return strToInt(&contentLengthStrBuf[0]);
+      return strToInt(contentLengthStrBuf);
     }
     i++;
   }
@@ -270,10 +258,10 @@ int getContentLength(char* rbuf, int rsize)
 }
 
 
-int getPercentageDone(int remaining, int full)
+word getPercentageDone(word remaining, word full)
 {
-  int x = remaining * 100;
-  return 100 - MATH_div(x, full);
+  word x = remaining * 100;
+  return 100 - MATH_divU(x, full);
 }
 
 // Receive response while connected
@@ -281,32 +269,32 @@ int getPercentageDone(int remaining, int full)
 void receiveResponse(char* path, char* fname)
 {
 
-  int firstResponse = 1;
-  int contentLengthOrig[1]; // its an array now, because of a compiler bug
+  word firstResponse = 1;
+  word contentLengthOrig[1]; // its an array now, because of a compiler bug in DeprCcompiler (ShivyC)
   contentLengthOrig[0] = 0;
-  int contentLength = 0;
+  word contentLength = 0;
 
-  int currentProgress = 0;
+  word currentProgress = 0;
 
   BDOS_PrintConsole("Downloading response (any key to stop)\n");
-  while (wizGetSockReg8(SOCKET_CLIENT, SnSR) == SOCK_ESTABLISHED)
+  while (wizGetSockReg8(SOCKET_CLIENT, WIZNET_SnSR) == WIZNET_SOCK_ESTABLISHED)
   {
-    int rsize = wizGetSockReg16(SOCKET_CLIENT, SnRX_RSR);
+    word rsize = wizGetSockReg16(SOCKET_CLIENT, WIZNET_SnRX_RSR);
     if (rsize != 0)
     {
       char* rbuf = (char*) HEAP_LOCATION; // we use heap for the receive data buffer
-      wizReadRecvData(SOCKET_CLIENT, &rbuf[0], rsize);
+      wizReadRecvData(SOCKET_CLIENT, rbuf, rsize);
       if (firstResponse)
       {
-        int dataStart = getDataStart(&rbuf[0], rsize);
-        contentLength = getContentLength(&rbuf[0], rsize);
+        word dataStart = getDataStart(rbuf, rsize);
+        contentLength = getContentLength(rbuf, rsize);
         contentLengthOrig[0] = contentLength;
 
         BDOS_PrintConsole("Status code: ");
-        BDOS_PrintDecConsole(getStatusCode(&rbuf[0], rsize));
+        BDOS_PrintDecConsole(getStatusCode(rbuf, rsize));
         BDOS_PrintConsole("\n");
 
-        if (!initOutputFile(&path[0], &fname[0], &rbuf[dataStart], rsize - dataStart))
+        if (!initOutputFile(path, fname, &rbuf[dataStart], rsize - dataStart))
           return;
 
         contentLength -= (rsize - dataStart);
@@ -319,7 +307,7 @@ void receiveResponse(char* path, char* fname)
       }
       else
       {
-        if (FS_writeFile(&rbuf[0], rsize) != ANSW_USB_INT_SUCCESS)
+        if (FS_writeFile(rbuf, rsize) != FS_ANSW_USB_INT_SUCCESS)
         {
           FS_close();
           BDOS_PrintConsole("Error while writing to file\n");
@@ -347,11 +335,11 @@ void receiveResponse(char* path, char* fname)
       break;
     }
   }
-  wizCmd(SOCKET_CLIENT, CR_DISCON);
+  wizCmd(SOCKET_CLIENT, WIZNET_CR_DISCON);
   BDOS_PrintConsole("Output written to ");
-  BDOS_PrintConsole(&path[0]);
+  BDOS_PrintConsole(path);
   BDOS_PrintConsole("/");
-  BDOS_PrintConsole(&fname[0]);
+  BDOS_PrintConsole(fname);
   BDOS_PrintConsole("\n");
   FS_close();
 }
@@ -360,16 +348,16 @@ void receiveResponse(char* path, char* fname)
 // convert string to IP by looking at the dots
 void strToIP(char* str, char* IP)
 {
-  int i = 0;
-  int dotsFound = 0;
-  int digitCounter = 0;
+  word i = 0;
+  word dotsFound = 0;
+  word digitCounter = 0;
   char tmp[16];
   while (str[i] != 0)
   {
     if (str[i] == '.')
     {
       tmp[digitCounter] = 0;
-      IP[dotsFound] = strToInt(&tmp[0]);
+      IP[dotsFound] = strToInt(tmp);
       dotsFound += 1;
       digitCounter = 0;
     }
@@ -382,7 +370,7 @@ void strToIP(char* str, char* IP)
     i++;
   }
   tmp[digitCounter] = 0;
-  IP[dotsFound] = strToInt(&tmp[0]);
+  IP[dotsFound] = strToInt(tmp);
 }
 
 
@@ -396,23 +384,23 @@ int main()
 
   // IP
   char ipstr[24];
-  BDOS_GetArgN(1, &ipstr[0]);
+  BDOS_GetArgN(1, ipstr);
   char ipTarget[4];
-  strToIP(&ipstr[0], &ipTarget[0]);
+  strToIP(ipstr, ipTarget);
 
   // Port
-  int portTarget = 0;
+  word portTarget = 0;
   char portstr[16];
-  BDOS_GetArgN(4, &portstr[0]);
+  BDOS_GetArgN(4, portstr);
   if (portstr[0] != 0)
-    portTarget = strToInt(&portstr[0]);
+    portTarget = strToInt(portstr);
 
   if (portTarget == 0)
       portTarget = 80;
 
 
   // Connect to target
-  if (!connectToTarget(&ipTarget[0], portTarget))
+  if (!connectToTarget(ipTarget, portTarget))
   {
     BDOS_PrintConsole("Could not connect to host\n");
     return 'q';
@@ -429,12 +417,12 @@ int main()
 
   // If no filename is given, use OUT.TXT as default
   char fname[16];
-  BDOS_GetArgN(3, &fname[0]);
+  BDOS_GetArgN(3, fname);
   if (fname[0] == 0)
   {
-    strcpy(&fname[0], "OUT.TXT");
+    strcpy(fname, "OUT.TXT");
   }
-  receiveResponse(&path[0], &fname[0]);
+  receiveResponse(path, fname);
 
 
   return 'q';
